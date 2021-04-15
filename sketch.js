5873a0ae55bd79ab7e5f92f0f6f84ae1d2c36330
genebit <genebitara@gmail.com>
2021-04-15 20:32:22 +0800

    Finished Double Pendulum👏

--- sketch.js
+++ sketch.js
@@ -1,51 +1,72 @@
+const gravity = 1;
+
+let mass1 = 10;
+let length1 = 100;
+let angle1 = 200;
+let xPos1 = 0;
+let yPos1 = 0;
+let angularVelocity1 = 0.01;
+
+let mass2 = 15;
+let length2 = 100;
+let angle2 = 200;
+let xPos2 = 0;
+let yPos2 = 0;
+let angularVelocity2 = 0.02;
+
 function setup() {
-  createCanvas(500, 500);
+     createCanvas(500, 500);
 }
 
-var angle1 = 0;
-var angle2 = 0;
-
 function draw() {
-  var mass1 = 15;
-  var length1 = 100;
-  var xEnd1 = length1 * sin(angle1);
-  var yEnd1 = length1 * cos(angle1);
+     // Center the elements
+     background(30, 30, 30);
+     translate(250, 150);
+     stroke(255);
+     strokeWeight(6);
 
-  const gravity = 9.8;
-  var angularVelocity1 = 0.01;
+     // Point origin
+     ellipse(0, 0, 10, 10);
 
-  var mass2 = 15;
-  var length2 = 100;
-  var xEnd2 = xEnd1 + length2 * sin(angle2);
-  var yEnd2 = yEnd1 + length2 * cos(angle2);
+     // First pendulum-------/
+     xPos1 = length1 * sin(angle1);
+     yPos1 = length1 * cos(angle1);
 
-  var angularVelocity2 = 0.01;
+     line(0, 0, xPos1, yPos1);
+     ellipse(xPos1, yPos1, mass1, mass1);
 
-  // Formula for angular velocity 1
-  let numerator1 = -gravity (2 (mass1 + mass2)) * sin(angle1);
-  let numerator2 = -mass2 * gravity * sin(angle1 - 2 (angle2));
-  let numerator3 = -2 * sin(angle1 - angle2);
-  let numerator4 = mass2 (angle2 * angle2 * length2 + angle1 * angle1 * length1 * cos(angle1 - angle2));
-  
-  let numerator = numerator1 + numerator2 + numerator3 + numerator4;
-  let denominator = length1 (2 (mass1 + mass2 - mass2 * cos(2 (angle1 - 2 * angle2))));
-  
-  // Center the elements
-  background(30, 30, 30);
-  translate(250, 150);
+     // Second pendulum-------/
+     xPos2 = xPos1 + length2 * sin(angle2);
+     yPos2 = yPos1 + length2 * cos(angle2);
 
-  // Point origin
-  ellipse(0, 0, 10, 10);
+     line(xPos1, yPos1, xPos2, yPos2);
+     ellipse(xPos2, yPos2, mass2, mass2);
 
-  // First pendulum-------/
-  stroke(255);
-  strokeWeight(8);
-  line(0, 0, xEnd1, yEnd1);
-  ellipse(xEnd1, yEnd1, mass1, mass1);
+     // Formula for computing angular velocity 1
+     let ang1Num = -gravity * (2 * mass1 + mass2) * sin(angle1) -
+                    mass2 * gravity * sin(angle1 - 2 * angle2) -
+                    2 * sin(angle1 - angle2) *
+                    mass2 * (angularVelocity2 * angularVelocity2 * length2 +
+                    angularVelocity1 * angularVelocity1 * length1 * cos(angle1 - angle2));
 
-  // Second pendulum-------/
-  stroke(255);
-  strokeWeight(8);
-  line(xEnd1, yEnd1, xEnd2, yEnd2);
-  ellipse(xEnd2, yEnd2, mass2, mass2);
+     let ang1Denom = length1 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2));
+     let angularAcceleration1 = ang1Num / ang1Denom;
+
+     // Formula for computing angular velocity 2
+     let ang2Num = 2 * sin(angle1 - angle2) * (angularVelocity1 * angularVelocity1 *
+                    length1 * (mass1 + mass2) + gravity * (mass1 + mass2) *
+                    cos(angle1) + angularVelocity2 * angularVelocity2 * length2 *
+                    mass2 * cos(angle1 - angle2));
+
+     let ang2Denom = length2 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2));
+     let angularAcceleration2 = ang2Num / ang2Denom;
+
+     angularVelocity1 += angularAcceleration1;
+     angularVelocity2 += angularAcceleration2;
+     angle1 += angularVelocity1;
+     angle2 += angularVelocity2;
+
+     // Damping
+     // angularVelocity1 *= 0.999;
+     // angularVelocity2 *= 0.999;
 }
